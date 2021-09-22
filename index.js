e2588b2e62e7860f4798df390b11458b52585947
jason <jmasters1000@yahoo.com>
2021-09-21 21:15:11 -0700

    Set up add js and index js in frontend

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
+const path = require('path');
+
 const app = express();
 
 // Connect to database
@@ -16,3 +18,9 @@ app.use('/api/url', require('./routes/url'));
 const PORT = 5000;
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
+
+// app.get('/', (req, res) => {
+//   app.use(express.static('frontend/build'));
+//   res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'));
+// })
