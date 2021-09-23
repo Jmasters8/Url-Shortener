ec257d3eb959d7c968f07a11bbf1cda0fa95c16d
jason <jmasters1000@yahoo.com>
2021-09-22 21:30:40 -0700

    Fix frontend root

--- frontend/src/components/root.js
+++ frontend/src/components/root.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import App from './app';
+import App from './App';
 
 
-const Root = ({ store }) => (
-  <Provider store={ store }>
-    <App />
-  </Provider>
+const Root = ({ store }) => ( 
+  <App />
 );
 
 export default Root;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import Root from './components/root';
+
 
 // ReactDOM.render(
 //   <React.StrictMode>
@@ -19,7 +20,3 @@ document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<Root store={store}/>, root)
 })
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
